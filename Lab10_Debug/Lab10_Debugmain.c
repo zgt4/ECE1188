29e12c5fc63b1f0dffd9c7f80b497ae7a8dee0a3
Jake Brooks <59630533+jcb156@users.noreply.github.com>
2021-03-19 14:34:50 -0400

    Update Lab10_Debugmain.c

--- Lab10_Debug/Lab10_Debugmain.c
+++ Lab10_Debug/Lab10_Debugmain.c
@@ -68,33 +68,105 @@ policies, either expressed or implied, of the FreeBSD Project.
 #include "..\inc\LaunchPad.h"
 #include "..\inc\FlashProgram.h"
 
+#define FLASH_BANK1_MIN     0x00020000  // Flash Bank1 minimum address
+#define FLASH_BANK1_MAX     0x0003FFFF  // Flash Bank1 maximum address
+
 
 void Debug_Init(void){
   // write this as part of Lab 10
+    //Initialize the bump sensors
+    // 0 2 3 5 6 7 all need to be initialized
+    P4->SEL0 &= ~0xED;
+    P4->SEL1 &= ~0xED;
+    P4->DIR &= ~0xED;
+    P4->REN |= 0xED;
+
+    //Illuminate the LEDs on P5.3 and P9.2
+    //Make P5.3 an output
+    P5->SEL0 &= 0xF7; //1111 0111
+    P5->SEL1 &= 0xF7;
+    P5->DIR |= 0x08; //0000 1000
+    P5->OUT &= 0xF7;
+    //Make P9.2 an output
+    P9->SEL0 &= 0xFB; //1111 1011
+    P9->SEL1 &= 0xFB;
+    P9->DIR |= 0x04;
+    P9->OUT &= 0xFB;
+    //Initialize the reflectance sensors
+    P7->SEL0 &= 0x00;
+    P7->SEL1 &= 0x00;
+    P7->DIR &= 0x00;
+
 }
 void Debug_Dump(uint8_t x, uint8_t y){
   // write this as part of Lab 10
+    static uint8_t bumpSensors;
+    static uint8_t LEDSensors;
+    uint8_t result;
+
+
+    //Capture the LED sensors current data
+    P5->OUT |= 0x08;      //Set P5.3 and P9.2 high
+    P9->OUT |= 0x04;
+
+    P7->DIR |= 0xFF;       // make P7.7-P7.0 out and pulse high for 10 us
+    P7->OUT |= 0xFF;
+    P7->REN |= 0xFF;      //Enable the pull up resistor
+    Clock_Delay1us(10);   // wait 10 us
+
+    P7->REN &= 0x00;
+    P7->DIR &= 0x00;       //Make the sensor pins input
+    Clock_Delay1us(1000); //Wait t us
+
+    result = P7->IN;     //Read the inputs
+    P5->OUT &= 0xF7;      //Set P5.3 and P9.2 low
+    P9->OUT &= 0xFB;
+
+    bumpSensors = P4->IN & 0xED;
+    LEDSensors = result;
 }
 void Debug_FlashInit(void){ 
   // write this as part of Lab 10
+    Flash_Init(48);
+    uint32_t address;
+    address = FLASH_BANK1_MIN;
+
+    while(address < FLASH_BANK1_MAX)
+    {
+        Flash_Erase(address);
+        address = address + 64;
+    }
 }
 void Debug_FlashRecord(uint16_t *pt){
   // write this as part of Lab 10
+    uint32_t address;
+    address = FLASH_BANK1_MIN;
+    while(*(uint32_t*)address != 0xFFFFFFFF){ // find first free block
+      address = address + 64;
+      if(address > FLASH_BANK1_MAX) //Flash is full
+      {
+          return;
+      }
+    }
+    Flash_FastWrite((uint32_t *)pt, address, 16); //Write 64 bytes
 }
 void SysTick_Handler(void){ // every 1ms
   // write this as part of Lab 10
 }
 
-int main(void){
+int Program10_0(void){
   // write this as part of Lab 10
-
+    Bump_Init();
+    EnableInterrupts();
   while(1){
   // write this as part of Lab 10
-
+      //WaitForInterrupt();
+      Bump_Read();
   }
 }
 
-int Program10_1(void){ uint8_t data=0;
+int Program10_1(void){
+  uint8_t data=0;
   Clock_Init48MHz();
   Debug_Init();
   LaunchPad_Init();
@@ -110,7 +182,7 @@ int Program10_1(void){ uint8_t data=0;
 // Driver test
 #define SIZE 256  // feel free to adjust the size
 uint16_t Buffer[SIZE];
-int Program10_2(void){ uint16_t i;
+int main(void){ uint16_t i;
   Clock_Init48MHz();
   LaunchPad_Init(); // built-in switches and LEDs
   for(i=0;i<SIZE;i++){
